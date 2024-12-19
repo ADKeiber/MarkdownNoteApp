08d75c966243627aff752d06644fd62f86e86694
Austin Keiber <adkeiber@gmail.com>
2024-12-19 05:18:29 -0500

    project init

---
+++ src/main/java/com/adk/markdownnoteapp/MarkdownNoteAppApplication.java
@@ -0,0 +1,13 @@
+package com.adk.markdownnoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownNoteAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownNoteAppApplication.class, args);
+	}
+
+}
