903a624e2d85a933256b91ea41bdfb87eb1728af
Austin Keiber <adkeiber@gmail.com>
2024-12-21 02:47:47 -0500

    finish simple user controller

--- src/main/java/com/adk/markdownnoteapp/controller/UserController.java
+++ src/main/java/com/adk/markdownnoteapp/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.adk.markdownnoteapp.controller;
 
+import com.adk.markdownnoteapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController("/user")
 public class UserController {
 
+    @Autowired
+    UserService userService;
+
     @PostMapping("/create/{username}")
     public ResponseEntity<Object> createUser(@RequestParam String username) {
-        return new ResponseEntity<>("It Worked", HttpStatus.OK);
+        return new ResponseEntity<>(userService.createUser(username), HttpStatus.OK);
     }
 
     @GetMapping("/getUserId/{username}")
     public ResponseEntity<Object> getUserId(@RequestParam String username) {
-        return new ResponseEntity<>("It Worked", HttpStatus.OK);
+        return new ResponseEntity<>(userService.getUserId(username), HttpStatus.OK);
     }
 }
