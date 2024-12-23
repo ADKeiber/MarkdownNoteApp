08b954366fc4221871093dfda447ae0e8a7a966d
Austin Keiber <adkeiber@gmail.com>
2024-12-23 18:27:07 -0500

    upload file

--- src/main/java/com/adk/markdownnoteapp/service/FileService.java
+++ src/main/java/com/adk/markdownnoteapp/service/FileService.java
@@ -1,16 +1,47 @@
 package com.adk.markdownnoteapp.service;
 
+import com.adk.markdownnoteapp.errorhandling.EntityNotFoundException;
 import com.adk.markdownnoteapp.model.FileType;
+import com.adk.markdownnoteapp.model.UserEntity;
+import com.adk.markdownnoteapp.model.UserFile;
+import com.adk.markdownnoteapp.repo.FileRepo;
+import com.adk.markdownnoteapp.repo.UserRepo;
+import org.apache.catalina.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.util.Optional;
 
 @Service
 public class FileService implements IFileService {
+
+    @Autowired
+    FileRepo fileRepo;
+
+    @Autowired
+    UserRepo userRepo;
+
     @Override
     public String uploadFile(MultipartFile file, String userId) {
-        return "";
+        Optional<UserEntity> userOptional = userRepo.findById(userId);
+
+        if(userOptional.isEmpty())
+            throw new EntityNotFoundException(UserEntity.class, "id", userId);
+
+        UserFile userFile = new UserFile();
+        try {
+            userFile.setData(file.getBytes());
+        } catch (Exception e) {
+            //Throw some sort of error
+        }
+
+        userFile.setUser(userOptional.get());
+
+        UserFile savedFile  = fileRepo.save(userFile);
+
+        return savedFile.getId();
     }
 
     @Override
