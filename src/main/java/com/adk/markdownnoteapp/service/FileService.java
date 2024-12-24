cff91e597a25e540c889ab52a1c97af39f11cdcb
Austin Keiber <adkeiber@gmail.com>
2024-12-24 01:56:26 -0500

    upload file

--- src/main/java/com/adk/markdownnoteapp/service/FileService.java
+++ src/main/java/com/adk/markdownnoteapp/service/FileService.java
@@ -6,12 +6,13 @@ import com.adk.markdownnoteapp.model.UserEntity;
 import com.adk.markdownnoteapp.model.UserFile;
 import com.adk.markdownnoteapp.repo.FileRepo;
 import com.adk.markdownnoteapp.repo.UserRepo;
-import org.apache.catalina.User;
+import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Optional;
 
 @Service
@@ -46,6 +47,24 @@ public class FileService implements IFileService {
 
     @Override
     public File getFileWithType(String fileId, FileType fileType) {
-        return null;
+        Optional<UserFile> fileOptional = fileRepo.findById(fileId);
+
+        if(fileOptional.isEmpty())
+            throw new EntityNotFoundException(UserFile.class, "id", fileId);
+
+        String fileName = switch(fileType) {
+            case FileType.MARKDOWN -> fileOptional.get().getId() + ".md";
+            case FileType.TXT -> fileOptional.get().getId() + ".txt";
+            case FileType.HTML -> fileOptional.get().getId() + ".html";
+            default -> throw new IllegalStateException("Unexpected value: " + fileType);
+        };
+        File file = new File(fileName);
+        try {
+            FileUtils.writeByteArrayToFile(file, fileOptional.get().getData());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return file;
     }
 }
