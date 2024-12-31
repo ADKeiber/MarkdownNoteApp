c5268dd2f56b814a7c837da886e4c4203acd57ee
Austin Keiber <adkeiber@gmail.com>
2024-12-30 20:59:22 -0500

    repo docs

--- src/main/java/com/adk/markdownnoteapp/repo/FileRepo.java
+++ src/main/java/com/adk/markdownnoteapp/repo/FileRepo.java
@@ -7,8 +7,17 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Repo used to interact with files
+ */
 @Repository
 public interface FileRepo extends ListCrudRepository<UserFile, String> {
+
+    /**
+     * Finds a list of files that are associated with a given userID
+     * @param userId {@link String} the user's ID
+     * @return {@link Optional} {@link List} of {@link UserFile} with a given user ID
+     */
     Optional<List<UserFile>> findByUserId(String userId);
 
 }
--- src/main/java/com/adk/markdownnoteapp/repo/UserRepo.java
+++ src/main/java/com/adk/markdownnoteapp/repo/UserRepo.java
@@ -6,8 +6,23 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+/**
+ * Repo used to interact with users in a database
+ */
 @Repository
 public interface UserRepo extends ListCrudRepository<UserEntity, String> {
+
+    /**
+     * Finds a user entity with the given username
+     * @param username {@link String} the user name
+     * @return {@link Optional} of {@link UserEntity} with the given username
+     */
     Optional<UserEntity> findByUsername(String username);
+
+    /**
+     * If a user exists with the given username
+     * @param username {@link String} the user's username
+     * @return {@code true} if user with given username exists, {@code false} otherwise
+     */
     Boolean existsByUsername(String username);
 }
